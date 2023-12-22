7370d20a9f81652077545a8dee3ac15a5c9c1592
Michael Mesfin <michael.21duke@gmail.com>
2023-12-22 10:19:36 +0300

    Enable page routing and understand how home and different page routes work

---
+++ src/app/about/page.js
@@ -0,0 +1,3 @@
+export default function About(){
+  return <h1 className="text-amber-400 font-semibold">Hello Luke</h1>;
+}
---
+++ src/app/profile/page.js
@@ -0,0 +1,3 @@
+export default function About(){
+  return <h1 className="text-emerald-400 font-semibold">Hello Asoka</h1>;
+}
