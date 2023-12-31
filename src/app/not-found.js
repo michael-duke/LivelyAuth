60b0f9a3fdee22629212bcdaaf6824e98840f601
Michael Mesfin <michael.21duke@gmail.com>
2023-12-31 19:24:12 +0300

    Implement custome 404 Page with not-found.js

---
+++ src/app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className=" h-screen flex flex-col items-center justify-center">
+      <h2 className="mx-auto font-semibold text-slate-700"><span className="mx-4">404 |</span>Page Not Found</h2>
+      <p>Could not find requested resource</p>
+    </div>
+  );
+}
