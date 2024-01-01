7ce981e7445a3344a631481d82e2c3863168afde
Michael Mesfin <michael.21duke@gmail.com>
2024-01-01 19:26:44 +0300

    Implement custom 404 page and connect notfound route from next/navigation

--- src/app/products/[productId]/reviews/[reviewid]/page.js
+++ src/app/products/[productId]/reviews/[reviewid]/page.js
@@ -1,4 +1,7 @@
+import { notFound } from 'next/navigation';
+
 export default function ReviewDetail({ params: { productId, reviewId } }) {
+  if (+reviewId > 1000) notFound();
   return (
     <h2>
       Review {reviewId} for product {productId}
