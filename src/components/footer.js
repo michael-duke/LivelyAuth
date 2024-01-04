6f76204f63407dcdd41159bc961017355d5a4a85
Michael Mesfin <michael.21duke@gmail.com>
2024-01-05 01:22:42 +0300

    Implement Header and Footer components

---
+++ src/components/footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="absolute bottom-0 right-0 left-0 flex justify-center border rounded-md border-purple-700 mx-4 mb-4 bg-purple-500 text-white">
+      Footer
+    </footer>
+  );
+}
---
+++ src/components/header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="flex justify-center border rounded-md border-red-700 mx-4 mb-4 bg-red-500 text-white">
+      Header
+    </header>
+  );
+}
