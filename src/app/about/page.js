a67c6f8dc7067c38d9a650a2793c8e1a72525396
Michael Mesfin <michael.21duke@gmail.com>
2024-01-08 07:49:44 +0300

    Implement absolute title metadata for About Page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,7 +1,17 @@
 export const metadata = {
-  title: 'About Lively',
+  title: {
+    absolute: 'About Lively',
+  },
 };
 
 export default function About() {
-  return <h1 className="text-amber-400 font-semibold">Hello Luke</h1>;
+  return (
+    <>
+      <div className="bg-green-300 border-2 border-purple-700 flex items-center p-3">
+        <span className="border">Hello</span>
+        <span className="ml-3 border">Darth Vader</span>
+      </div>
+      <h1 className="text-amber-400 font-semibold">Hello Luke</h1>
+    </>
+  );
 }
