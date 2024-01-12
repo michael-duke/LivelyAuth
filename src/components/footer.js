572a98ca9a2b9f3023a715e0c5e081964c2a908a
Michael Mesfin <michael.21duke@gmail.com>
2024-01-12 09:28:44 +0300

    Adjust footer styles

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,9 @@
 export default function Footer() {
   return (
-    <footer className="absolute bottom-0 right-0 left-0 flex justify-center border rounded-md border-purple-700 mx-4 mb-4 bg-purple-500 text-white">
-      Footer
-    </footer>
+    <div className="flex flex-col min-h-screen">
+      <footer className="fixed bottom-0 right-0 left-0 flex justify-center border rounded-md border-purple-700 mx-4 mb-4 bg-purple-500 text-white">
+        Footer
+      </footer>
+    </div>
   );
 }
