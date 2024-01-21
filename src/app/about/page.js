3f58a87b9de4b17f98f78ebf981a90b83e032bdd
Michael Mesfin <michael.21duke@gmail.com>
2024-01-21 22:52:35 +0300

    Implement Routing metadata

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,3 +1,7 @@
-export default function About(){
+export const metadata = {
+  title: 'About Lively',
+};
+
+export default function About() {
   return <h1 className="text-amber-400 font-semibold">Hello Luke</h1>;
 }
--- src/app/products/[productId]/page.js
+++ src/app/products/[productId]/page.js
@@ -1,3 +1,12 @@
+export const generateMetadata = async ({ params: { productId } }) => {
+  const title = await new Promise((resolve) => {
+    setTimeout(() => resolve(`Google Pixel ${productId}`), 2000);
+  });
+  return {
+    title: `Product # ${title}`,
+  };
+};
+
 export default function ProductDetails({ params: { productId } }) {
   return <h1>Details about Product {productId}</h1>;
 }
