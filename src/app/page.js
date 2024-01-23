ba5a7897976bce8c6332ce4c7d657e06d5322210
Michael Mesfin <michael.21duke@gmail.com>
2024-01-23 08:38:16 +0300

    Implement Link component and navigation

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,21 @@
+import Link from 'next/link';
+
 export default function Home() {
-  return <main>Hello Obi Wan</main>;
+  return (
+    <main>
+      <h1>Hello Obi Wan</h1>
+      <Link
+        href="/blog"
+        className="underline underline-offset-2 decoration-slate- block mt-2"
+      >
+        Blog
+      </Link>
+      <Link
+        href="/products"
+        className="underline underline-offset-2 decoration-slate-600 block mt-2"
+      >
+        Products
+      </Link>
+    </main>
+  );
 }
--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 export const metadata = {
   title: 'Products',
 };
@@ -5,9 +6,9 @@ export const metadata = {
 export default function ProductList() {
   return <>
   <ul className="list-disc flex flex-col gap-3 ml-6">
-    <li className="cursor-pointer">Pr1</li>
-    <li className="cursor-pointer">Pr2</li>
-    <li className="cursor-pointer">Pr3</li>
+    <li className="cursor-pointer"><Link href="/products/1">Pr1</Link></li>
+    <li className="cursor-pointer"><Link href="/products/2">Pr2</Link></li>
+    <li className="cursor-pointer"><Link href="/products/3" replace>Pr3</Link></li>
   </ul>
   </>;
 }
