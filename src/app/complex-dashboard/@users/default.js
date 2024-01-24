7a61ada3a3672f2bb8ae41221a7368b1a76da4a1
Michael Mesfin <michael.21duke@gmail.com>
2024-01-24 09:49:24 +0300

    Implement fallback default components for revenues, children and users slots

---
+++ src/app/complex-dashboard/@users/default.js
@@ -0,0 +1,10 @@
+import Card from '@/components/card';
+
+export default function DefaultUserAnalytics() {
+  return (
+    <Card className="bg-green-500 text-white p-4 border flex items-center">
+      {' '}
+      User Analytics
+    </Card>
+  );
+}
---
+++ src/app/complex-dashboard/default.js
@@ -0,0 +1,3 @@
+export default function DefaultComplexDashboard() {
+  return <h1>Complex Dashboard Page</h1>;
+}
